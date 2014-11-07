6d8205281795cb2c37c20a5ba6415844b75e2a09
John Bubriski <bubriski@gmail.com>
2014-11-07 12:53:55 -0500

    Adds player, platform, and bouncing/moving

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour
+{
+    public int JumpForce = 100;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2d)
+    {
+        collision2d.rigidbody.AddForce(new Vector2(0, JumpForce));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public int MoveForce = 10;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody2D.AddForce(new Vector2(-MoveForce * Time.deltaTime * 10, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody2D.AddForce(new Vector2(MoveForce * Time.deltaTime * 10, 0));
+        }
+    }
+}
