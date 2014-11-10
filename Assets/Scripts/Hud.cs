03d1092897e41cefef8a5684f9d46c91d4ae4f68
John Bubriski <bubriski@gmail.com>
2014-11-10 09:22:41 -0500

    Adds distance tracking to HUD

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hud : MonoBehaviour
+{
+	private float _distanceTraveled = 0;
+	private Transform _player;
+
+	public float DistanceScale = 100;
+
+	void Start ()
+	{
+		_player = GameObject.Find ("Player").transform;
+	}
+
+	void Update ()
+	{
+		if (_player.position.y * DistanceScale > _distanceTraveled)
+			_distanceTraveled = (int)(_player.position.y * DistanceScale);
+	}
+
+	void OnGUI ()
+	{
+		GUI.Label (new Rect (10, 10, 100, 20), _distanceTraveled.ToString ());
+	}
+}
